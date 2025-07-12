0f3f7a3cf842a0657957041b42922a9ced777b8b
Akhil Mahesh <akhilmahesh@proton.me>
2025-07-12 21:49:53 +0530

    feat: Initial Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-primary">
+        <button className="bg-accent hover:bg-highlight text-dark">Order Now</button>
+      </div>
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2e7d32',
+        accent: '#81c784',
+        bg: '#fdfaf4',
+        card: '#f1f8e9',
+        highlight: '#f9a825',
+        dark: '#212121',
+      },
+    },
+  },
+  plugins: [],
+}
